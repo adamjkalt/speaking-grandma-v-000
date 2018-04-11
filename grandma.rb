2468c59f65e7e5356581cabc784d64579bb92188
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-11 03:35:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(phrase)
   if phrase.upcase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOUR GRANDMA!"
+  elsif "I LOVE YOUR GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP, SONNY!"
